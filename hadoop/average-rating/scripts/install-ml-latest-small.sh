0fc9cc9b8ac55db7f03bdb629fc1b4b15a611901
Jeremy Audet <jerebear@protonmail.com>
2018-09-13 18:52:05 -0400

    Make population of data/ more efficient
    Don't copy archives into data/ directory before expanding them. Instead,
    expand them directly from ~/.cache/average-data/ into data/.

--- hadoop/average-rating/scripts/install-ml-latest-small.sh
+++ hadoop/average-rating/scripts/install-ml-latest-small.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 # coding=utf-8
 #
-# Install data/ml-latest-small.zip.
+# Install data/ml-latest-small.
 set -euo pipefail
 
-readonly src="$(
+readonly src_archive="$(
     realpath --canonicalize-missing ~/.cache/average-rating/ml-latest-small.zip
 )"
-readonly dst="$(realpath --canonicalize-missing "data/$(basename "${src}")")"
+readonly dst_dir="$(realpath --canonicalize-existing data)"
 
 # TODO: Download into /tmp/..., then move to ~/.cache/..., thus preventing
 # partial downloads.
-if [ ! -e "${src}" ]; then
-    mkdir -p "$(dirname "${src}")"
-    curl -o "${src}" \
+if [ ! -e "${src_archive}" ]; then
+    mkdir -p "$(dirname "${src_archive}")"
+    curl -o "${src_archive}" \
         http://files.grouplens.org/datasets/movielens/ml-latest-small.zip
 fi
-if [ ! -e "${dst}" ]; then
-    install -Dm640 "${src}" "${dst}"
-fi
+unzip -d "${dst_dir}" "${src_archive}"
